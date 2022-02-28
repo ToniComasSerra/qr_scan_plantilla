2b516f025e57c557d113070d3ee4d652b2b72677
ToniComasSerra <61323246+ToniComasSerra@users.noreply.github.com>
2022-02-28 19:42:57 +0100

    Video 9 acabado

--- lib/screens/mapa_screen.dart
+++ lib/screens/mapa_screen.dart
@@ -1,14 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:qr_scan/models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapaScreen extends StatelessWidget {
+class MapaScreen extends StatefulWidget {
   const MapaScreen({Key? key}) : super(key: key);
 
   @override
+  State<MapaScreen> createState() => _MapaScreenState();
+}
+
+class _MapaScreenState extends State<MapaScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  @override
   Widget build(BuildContext context) {
+    final CameraPosition _puntInical = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
     final ScanModel scan = ModalRoute.of(context)!.settings.arguments as ScanModel;
     return Scaffold(
-      body: Center(child: Text('${scan.valor}')),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _puntInical,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
