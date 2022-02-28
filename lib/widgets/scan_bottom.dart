13a9e1fe07456ff114e30ab7198913b8bbab5a01
ToniComasSerra <61323246+ToniComasSerra@users.noreply.github.com>
2022-02-28 20:53:16 +0100

    Video 11 acabado

--- lib/widgets/scan_bottom.dart
+++ lib/widgets/scan_bottom.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_scan/providers/scan_list_provider.dart';
 import 'package:qr_scan/utils/utils.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 
 import '../models/scan_model.dart';
 import '../providers/db_provider.dart';
@@ -16,10 +18,14 @@ class ScanButton extends StatelessWidget {
       child: const Icon(
         Icons.filter_center_focus,
       ),
-      onPressed: () {
+      onPressed: () async {
         print('Botó polsat!');
-        //String barcodeScanRes = 'https://paucasesnovescifp.cat/';
-        String barcodeScanRes = 'geo:39.7260847,2.9113035';
+        // String barcodeScanRes = 'https://paucasesnovescifp.cat/';
+        // String barcodeScanRes = 'geo:39.7260847,2.9113035';
+
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#3D8BEF', 'Cancel', false, ScanMode.QR);
+        print(barcodeScanRes);
+                                                    
         final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
         ScanModel nouScan = ScanModel(valor: barcodeScanRes);
         scanListProvider.nouScan(barcodeScanRes);
